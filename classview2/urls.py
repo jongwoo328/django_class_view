b27d52cf16f9cba1137e57f806447f18857a7ebf
JongWoo <jongwoo328@gmail.com>
2020-08-16 22:09:36 +0900

    media url 세팅

--- classview2/urls.py
+++ classview2/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -25,4 +27,4 @@ urlpatterns = [
     path('api/articles/', include('articles.urls')),
     path('api/accounts/', include('accounts.urls')),
     path('images/', include('images.urls')),
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
